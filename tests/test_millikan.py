1dad5117339074320e1ee0509b31e4f044c0fb02
zhusiyuv <1477117691@qq.com>
2025-03-20 17:12:57 +0800

    Update test_millikan.py

--- tests/test_millikan.py
+++ tests/test_millikan.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from millikan_fit import load_data, calculate_parameters, calculate_planck_constant
+from src.millikan_fit import load_data, calculate_parameters, calculate_planck_constant
 
 class TestMillikanFit(unittest.TestCase):
     def test_load_data(self):
@@ -10,7 +10,7 @@ class TestMillikanFit(unittest.TestCase):
 
     def test_calculate_parameters(self):
         x = np.array([1, 2, 3, 4, 5])
-        y = np.array([2, 4, 6, 8])
+        y = np.array([2, 4, 6, 8, 10])
         m, c, Ex, Ey, Exx, Exy = calculate_parameters(x, y)
         self.assertAlmostEqual(m, 2.0)
         self.assertAlmostEqual(c, 0.0)
