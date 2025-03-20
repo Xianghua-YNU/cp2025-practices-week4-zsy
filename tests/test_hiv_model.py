7f7a269091051e1e909db902f34e0379e1bde622
zhusiyuv <1477117691@qq.com>
2025-03-20 16:56:38 +0800

    Update test_hiv_model.py

--- tests/test_hiv_model.py
+++ tests/test_hiv_model.py
@@ -1,23 +1,18 @@
 import unittest
 import numpy as np
 from src.hiv_model_student import HIVModel, load_hiv_data
-#from solutions.hiv_model_solution import HIVModel, load_hiv_data
 
 class TestHIVModel(unittest.TestCase):
-    def test_model_initialization(self):
-        model = HIVModel(A=1000, alpha=0.5, B=500, beta=0.1)
-        self.assertIsNotNone(model)
-
-    def test_viral_load_calculation(self):
-        model = HIVModel(A=1000, alpha=0.5, B=500, beta=0.1)
+    def test_viral_load(self):
+        model = HIVModel(A=1000, alpha=0.1, B=500, beta=0.05)
         time = np.linspace(0, 10, 100)
         result = model.viral_load(time)
         self.assertEqual(len(result), 100)
 
     def test_data_loading(self):
-        time, load = load_hiv_data('data/HIVseries.csv')
+        time, response = load_hiv_data('data/HIVseries.csv')
         self.assertGreater(len(time), 0)
-        self.assertGreater(len(load), 0)
+        self.assertGreater(len(response), 0)
 
 if __name__ == "__main__":
     unittest.main()
